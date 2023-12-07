10b01030c9800565774e9907f1a474db9e71afae
DevMuthuKumar <muthuk@crayondata.com>
2023-12-07 12:03:01 +0530

    Port added

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ mongoose
   })
   .then(() => {
     // Start the server
-    app.listen(3000, () => {
-      console.log("Server running on port 3000");
+    app.listen(8080, () => {
+      console.log("Server running on port 8080");
     });
   })
   .catch((error) => {
